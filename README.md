be412674e2ef85a1f7e850982bed2f9a362fc26d
Maciej Halber <macikuh@gmail.com>
2017-05-12 18:01:45 -0400

    Cleaned up example

--- README.md
+++ README.md
@@ -3,13 +3,14 @@
 Single-file C11 libraries under the public-domain license. Header file for
 each library contains documentation. Each libary also contains an accompanying
 example file showcasing its use. 
-Note that anything below version 1.0 have not been well tested yet.
+Note that anything below version 1.0 have not been well tested and api is subject
+to change.
 
 library             |  version   | description  
 :-------------------|:----------:|:---------------------------------------------
 **msh_argparse.h**  |    0.5     | command-line argument parsing
 **msh_vec_math.h**  |    0.5     | glsl inspired vector math (vectors, matrices and quaternions)
-**msh_gfx.h**       |    0.3     | OpenGL wrapper
+**msh_gfx.h**       |    0.35    | openGL wrapper
 **msh_cam.h**       |    0.2     | various types of camera
 **msh.h**           |    0.1     | standard library functionality
 
--- examples/msh_example.c
+++ examples/msh_example.c
@@ -2,60 +2,6 @@
 #include <stdlib.h>
 #include "msh.h"
 
-
-
-// #define gb_array_set_capacity(x, capacity) do { \
-// 	if (x) { \
-// 		void **gb__array_ = cast(void **)&(x); \
-// 		*gb__array_ = gb__array_set_capacity((x), (capacity), gb_size_of(*(x))); \
-// 	} \
-// } while (0)
-
-// // NOTE(bill): Do not use the thing below directly, use the macro
-// GB_DEF void *gb__array_set_capacity(void *array, isize capacity, isize element_size);
-
-
-// // TODO(bill): Decide on a decent growing formula for gbArray
-// #define gb_array_grow(x, min_capacity) do { \
-// 	isize new_capacity = GB_ARRAY_GROW_FORMULA(gb_array_capacity(x)); \
-// 	if (new_capacity < (min_capacity)) \
-// 		new_capacity = (min_capacity); \
-// 	gb_array_set_capacity(x, new_capacity); \
-// } while (0)
-
-
-// #define gb_array_append(x, item) do { \
-// 	if (gb_array_capacity(x) < gb_array_count(x)+1) \
-// 		gb_array_grow(x, 0); \
-// 	(x)[gb_array_count(x)++] = (item); \
-// } while (0)
-
-// #define gb_array_appendv(x, items, item_count) do { \
-// 	gbArrayHeader *gb__ah = GB_ARRAY_HEADER(x); \
-// 	GB_ASSERT(gb_size_of((items)[0]) == gb_size_of((x)[0])); \
-// 	if (gb__ah->capacity < gb__ah->count+(item_count)) \
-// 		gb_array_grow(x, gb__ah->count+(item_count)); \
-// 	gb_memcopy(&(x)[gb__ah->count], (items), gb_size_of((x)[0])*(item_count));\
-// 	gb__ah->count += (item_count); \
-// } while (0)
-
-
-
-// #define gb_array_pop(x)   do { GB_ASSERT(GB_ARRAY_HEADER(x)->count > 0); GB_ARRAY_HEADER(x)->count--; } while (0)
-// #define gb_array_clear(x) do { GB_ARRAY_HEADER(x)->count = 0; } while (0)
-
-// #define gb_array_resize(x, new_count) do { \
-// 	if (GB_ARRAY_HEADER(x)->capacity < (new_count)) \
-// 		gb_array_grow(x, (new_count)); \
-// 	GB_ARRAY_HEADER(x)->count = (new_count); \
-// } while (0)
-
-
-// #define gb_array_reserve(x, new_capacity) do { \
-// 	if (GB_ARRAY_HEADER(x)->capacity < (new_capacity)) \
-// 		gb_array_set_capacity(x, new_capacity); \
-// } while (0)
-
 typedef struct S {
   int a;
   int b;
@@ -85,7 +31,7 @@ int main()
   printf("Clamping -1.5f to 0-1 range: %f\n", msh_clamp01(-1.5));
   printf("Clamping 259 to 0-255 range: %d\n", msh_clamp(259, 0, 255));
   printf("Clamping -59 to 0-255 range: %d\n", msh_clamp(-59, 0, 255));
-  
+
   // ARRAY test
   msh_array(int32_t) array = NULL;
   msh_array_init( array, 5 );
